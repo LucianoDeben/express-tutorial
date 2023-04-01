d52461dfac6258bfa463e75a38a374d2ed88dd5b
LucidCodeAI <lutchocode@gmail.com>
2023-04-01 19:17:06 +0200

    Used express

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
-app.use(express.static("./public"));
-
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
-
-app.all("*", (req, res) => {
-  res.status(404).send("Page not found");
+  res.json({ name: "Luciano" }, { name: "Susan" });
 });
 
 app.listen(5000, () => {
